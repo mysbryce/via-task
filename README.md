9daa7e7b6869e2f988212b105d5f5dd471741589
mysbryce <123440999+mysbryce@users.noreply.github.com>
2025-05-11 09:07:30 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# via-task
+An open source desktop application, made for managing your tasks with your team easier
---
+++ src/renderer/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+    return (
+        <main className="flex flex-col gap-2 p-6">
+            Started project
+        </main>
+    )
+}
